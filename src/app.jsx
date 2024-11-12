c5d1a32dacc48d9d7569fe3c27736449a308259f
ReinnFall <jamestock18@gmail.com>
2024-11-12 12:52:15 -0700

    step 3 setting up react - app jsx/ css isnt displaying properly but added header and footer

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>; /*just copy pasted this - change as needed*/
+    return(
+        <><header>
+            <nav>
+                <a href="index.html">Home</a>
+                <a href="findStrings.html">Find Strings</a>
+                <a href="profile.html">Profile</a>
+            </nav>
+            <div className="login">Login:
+                <form method="get" action="profile.html">
+                    <label for="name"></label>
+                    <input type="text" id="name" placeholder="Your name here" />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        </header> 
+        <main> All app components</main>
+        <footer>
+            <a href="https://github.com/ReinnFall/CS260-Startup-Project.git">Github - James Stock</a>
+        </footer>
+        </>
+    );
 }
